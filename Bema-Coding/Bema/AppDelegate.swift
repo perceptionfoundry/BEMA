5472ed446ee80db91f961625887371a76a0bb63b
ShahRukh <shahaider@gmail.com>
2019-10-01 12:54:25 +0500

    snap kit login DONE

--- Bema-Coding/Bema/AppDelegate.swift
+++ Bema-Coding/Bema/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SCSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
+    
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+          
+          return SCSDKLoginClient.application(app, open: url, options: options)
+      }
+    
+    
+    
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
