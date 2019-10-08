4a0856de1fce349dcd98cafdcdfb9ff7606ce65e
ShahRukh <shahaider@gmail.com>
2019-10-08 22:04:35 +0500

    setting part DONE

---
+++ Bema-Coding/Bema/View/Controller/Settings/InviteVC.swift
@@ -0,0 +1,26 @@
+//
+//  InviteVC.swift
+//  Bema
+//
+//  Created by Syed ShahRukh Haider on 08/10/2019.
+//  Copyright © 2019 Syed ShahRukh Haider. All rights reserved.
+//
+
+import UIKit
+
+class InviteVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //******** OUTLET ACTION
+         @IBAction func noButtonAction(_ sender: Any) {
+
+                  self.dismiss(animated: true, completion: nil)
+               
+                }
+
+}
---
+++ Bema-Coding/Bema/View/Controller/Settings/LogoutVc.swift
@@ -0,0 +1,27 @@
+//
+//  LogoutVc.swift
+//  Bema
+//
+//  Created by Syed ShahRukh Haider on 08/10/2019.
+//  Copyright © 2019 Syed ShahRukh Haider. All rights reserved.
+//
+
+import UIKit
+
+class LogoutVc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ //******** OUTLET ACTION
+      @IBAction func noButtonAction(_ sender: Any) {
+
+               self.dismiss(animated: true, completion: nil)
+            
+             }
+
+}
