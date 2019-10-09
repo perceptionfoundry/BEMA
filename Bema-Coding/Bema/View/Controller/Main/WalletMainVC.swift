d1982c2dadfccf70749dab16730d93b546604e6a
ShahRukh <shahaider@gmail.com>
2019-10-09 23:32:11 +0500

    wallet screen commence

--- Bema-Coding/Bema/View/Controller/Main/WalletMainVC.swift
+++ Bema-Coding/Bema/View/Controller/Main/WalletMainVC.swift
@@ -10,13 +10,62 @@ import UIKit
 
 class WalletMainVC: UIViewController {
 
+    
+    @IBOutlet weak var walletListView: Custom_View!
+    
+    @IBOutlet weak var walletListViewConstant: NSLayoutConstraint!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.tabBarController?.tabBar.isHidden = true
+        
+        
+//        let scrollWalletView = UIPanGestureRecognizer(target: self, action: #selector(MoveUp))
+        
+    let tapWalletView = UITapGestureRecognizer(target: self, action: #selector(MoveUp))
+        self.walletListView.addGestureRecognizer(tapWalletView)
 
     }
     
+    @objc func MoveUp(){
+        
+        print("yes")
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.1) {
+                        self.walletListViewConstant.constant += -20
+
+            DispatchQueue.main.asyncAfter(deadline: .now()+0.1) {
+                                   self.walletListViewConstant.constant += -20
+
+                   }
+            DispatchQueue.main.asyncAfter(deadline: .now()+0.1) {
+                                   self.walletListViewConstant.constant += -20
+
+                   }
+            DispatchQueue.main.asyncAfter(deadline: .now()+0.1) {
+                                   self.walletListViewConstant.constant += -20
+
+                   }
+            DispatchQueue.main.asyncAfter(deadline: .now()+0.1) {
+                            self.walletListViewConstant.constant += -20
+
+            }
+        }
+        
+//        UIView.animate(withDuration: 2) {
+//            self.walletListViewConstant.constant = -120
+//        }
+//        }
+//        else if moveStatus.direction == .down{
+//
+//            self.walletListViewConstant.constant = -30
+//        }
+//
+    }
     @IBAction func chatButtonAction(_ sender: Any) {
         
         self.tabBarController?.selectedIndex = 0
