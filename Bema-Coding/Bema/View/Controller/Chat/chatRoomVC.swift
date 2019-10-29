98f8858fd7cf3242df08a95ba49c08cb85fb74cf
ShahRukh <shahaider@gmail.com>
2019-10-29 23:50:13 +0500

    chat textview

--- Bema-Coding/Bema/View/Controller/Chat/chatRoomVC.swift
+++ Bema-Coding/Bema/View/Controller/Chat/chatRoomVC.swift
@@ -8,21 +8,69 @@
 
 import UIKit
 
-class chatRoomVC: UIViewController {
+class chatRoomVC: UIViewController, UITextViewDelegate {
 
     
     @IBOutlet weak var ChatTextField : UITextView!
+    @IBOutlet weak var textViewHeight: NSLayoutConstraint!
     
-    
+    @IBOutlet weak var chatMsg_view: UIView!
+     @IBOutlet weak var navi_view: UIView!
+    var maxheight:CGFloat = 80
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
         
-    
+        print(navi_view.center.y)
+            print(chatMsg_view.center.y)
+        
+        ChatTextField.delegate = self
+        
+        ChatTextField.textColor = UIColor(red: 0.073, green: 0.624, blue: 0.616, alpha: 1)
+        
     }
     
 
+    
+    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+
+        if textView.text == "Say Something..." {
+            textView.text = ""
+            ChatTextField.textColor = UIColor.black
+
+        }
+        
+        print(navi_view.center.y)
+        print(chatMsg_view.center.y)
+            
+        
+
+        return true
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+           
+ 
+        if ChatTextField.contentSize.height < maxheight{
+        textViewHeight.constant = ChatTextField.contentSize.height
+        }
+
+        return true
+
+    }
+    
+    func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
+        
+    if textView.text == "" {
+        textView.text = "Say Something..."
+        ChatTextField.textColor = UIColor(red: 0.073, green: 0.624, blue: 0.616, alpha: 1)
+
+    }
+     print(navi_view.center.y)
+     print(chatMsg_view.center.y)
+        
+        return true
+    }
 
 }
